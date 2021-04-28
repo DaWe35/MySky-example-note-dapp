1d2af66c3df80b22f68890f3e0cd51aed730ecf0
DaWe <willteam35@gmail.com>
2021-04-28 20:00:50 +0200

    finalize

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 
 import { SkynetClient } from "skynet-js"
-import { ContentRecordDAC } from "@skynetlabs/content-record-library";
 
 
 let isLoggedIn = false
 let mySky
 let userId
 
-const hostApp = "sky-note.hns"
+const hostApp = "23672318746.hns"
 const client = new SkynetClient("https://siasky.net/")
 
-async function login() {
+async function loginButton() {
 	if (!isLoggedIn) {
 		isLoggedIn = await mySky.requestLoginAccess()
 	}
-	checkLogin()
+	await checkLogin()
+	await changeUI()
 }
 
 // Assume we have a logged-in mysky instance from above
 async function getJSON() {
 	try {
 		// Get discoverable JSON data from the given path.
-		const { data, skylink } = await mySky.getJSON("app.hns/path/file.json");
+		const { data, skylink } = await mySky.getJSON(hostApp + "/note.json");
 		return data
 	} catch (error) {
 		console.log(error)
@@ -33,11 +33,12 @@ async function setJSON(message) {
 	try {
 		console.log(message)
 		// Set discoverable JSON data at the given path. The return type is the same as getJSON.
-		const { data, skylink } = await mySky.setJSON("app.hns/path/file.json", { message: ''+message+'' })
+		const { data, skylink } = await mySky.setJSON(hostApp + "/note.json", { message: ''+message+'' })
 		return data
 	} catch (error) {
 	  	console.log(error)
-		  return false
+	  	alert('Error: ' + error.message)
+		return false
 	}
 }
 
@@ -45,8 +46,15 @@ async function checkLogin() {
 	isLoggedIn = await mySky.checkLogin();
 
 	if (isLoggedIn) {
-		await loadData()
 		userId = await mySky.userID()
+	} else {
+		userId = null
+	}
+}
+
+async function changeUI() {
+	if (isLoggedIn) {
+		await loadData()
 		$(".hide-if-logged-in").hide()
 		$(".show-if-logged-in").show()
 	} else {
@@ -57,12 +65,11 @@ async function checkLogin() {
 }
 
 async function loadData() {
-	let dacfile = await getJSON()
-	if (dacfile != null) {
-		$("#note").val(dacfile.message)
+	let dataFile = await getJSON()
+	if (dataFile != null) {
+		$("#note").val(dataFile.message)
 	} else {
 		$("#note").val('')
-		alert('failed to load')
 	}
 }
 
@@ -86,8 +93,6 @@ $("#save_note").click(function() {
 		if (data) {
 			$(".no-send").show()
 			$(".sending").hide()
-		} else {
-			alert('save failed')
 		}
 	})
 });
@@ -97,7 +102,7 @@ $("#logout").click(function() {
 });
 
 $("#login-button").click(function() {
-	login()
+	loginButton()
 });
 
 
@@ -108,14 +113,11 @@ async function initMySky() {
 		// needed for permissions write
 		mySky = await client.loadMySky(hostApp, {
 			debug: false,
-			dev: true,
+			dev: false,
 		});
   
 		// load necessary DACs and permissions
 		// await mySky.loadDacs(contentRecord);
-		const dac = new ContentRecordDAC()
-		let dacData = await mySky.loadDacs(dac)
-		console.log('dacData', dacData)
 	
 		// check if user is already logged in with permissions
 	} catch (e) {
@@ -133,6 +135,7 @@ async function initMySky() {
 	$(".hide-if-initialized").hide()
 	$(".show-if-initialized").show()
 	await checkLogin()
+	await changeUI()
 
 })();
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	},
 	optimization: {
 		// minimize: true,
-		minimize: false,
+		minimize: true,
 		minimizer: [
 			new TerserPlugin({
 				extractComments: false,

0f449797406848254d150caecab51208f63472ed
DaWe <willteam35@gmail.com>
2021-04-28 17:18:09 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { SkynetClient } from "skynet-js"
 import { ContentRecordDAC } from "@skynetlabs/content-record-library";
 
 
-
-
 let isLoggedIn = false
 let mySky
 let userId
@@ -12,48 +10,59 @@ let userId
 const hostApp = "sky-note.hns"
 const client = new SkynetClient("https://siasky.net/")
 
+async function login() {
+	if (!isLoggedIn) {
+		isLoggedIn = await mySky.requestLoginAccess()
+	}
+	checkLogin()
+}
 
-
-async function loadDacsExample() {
+// Assume we have a logged-in mysky instance from above
+async function getJSON() {
 	try {
-		// Initialize DAC, auto-adding permissions.
-		const dac = new ContentRecordDAC()
-		let data = await mySky.loadDacs(dac)
+		// Get discoverable JSON data from the given path.
+		const { data, skylink } = await mySky.getJSON("app.hns/path/file.json");
+		return data
+	} catch (error) {
+		console.log(error)
+		return null
+	}
+}
+
+async function setJSON(message) {
+	try {
+		console.log(message)
+		// Set discoverable JSON data at the given path. The return type is the same as getJSON.
+		const { data, skylink } = await mySky.setJSON("app.hns/path/file.json", { message: ''+message+'' })
 		return data
 	} catch (error) {
 	  	console.log(error)
+		  return false
 	}
 }
 
-
-
-async function requestLoginAccessExample() {
-	try {
-		mySky= await client.loadMySky(hostApp);
-  
-		isLoggedIn = await mySky.checkLogin();
-  
-	  // Add button action for login.
-	  if (!isLoggedIn) {
-		document.getElementById("login-button").addEventListener("click", function() {
-			const status =  mySky.requestLoginAccess()
-			if(status){
-				$(".hide-if-logged-in").hide()
-				$(".show-if-logged-in").show()
-			}
-		  });
-	  }
-	} catch (error) {
-	  console.log(error)
-	}
-}
-
-async function login() {
-	isLoggedIn = await mySky.requestLoginAccess()
+async function checkLogin() {
+	isLoggedIn = await mySky.checkLogin();
 
 	if (isLoggedIn) {
+		await loadData()
 		userId = await mySky.userID()
-		loadDacsExample()
+		$(".hide-if-logged-in").hide()
+		$(".show-if-logged-in").show()
+	} else {
+		userId = null
+		$(".hide-if-logged-in").show()
+		$(".show-if-logged-in").hide()
+	}
+}
+
+async function loadData() {
+	let dacfile = await getJSON()
+	if (dacfile != null) {
+		$("#note").val(dacfile.message)
+	} else {
+		$("#note").val('')
+		alert('failed to load')
 	}
 }
 
@@ -62,83 +71,68 @@ async function logout() {
 
 	isLoggedIn = false
 	userId = null
+
+	$(".show-if-logged-in").hide()
+	$(".hide-if-logged-in").show()
 }
 
-// Assume we have a logged-in mysky instance from above
-
-async function getJSONExample() {
-	try {
-	  // Get discoverable JSON data from the given path.
-	  const { data, skylink } = await mySky.getJSON("app.hns/path/file.json");
-	  return data
-
-	} catch (error) {
-	  console.log(error)
-	  return null
-	}
-return null
-  }
-
-async function setJSONExample(message) {
-	try {
-		console.log(message)
-	  // Set discoverable JSON data at the given path. The return type is the same as getJSON.
-	  const { data, skylink } = await mySky.setJSON("app.hns/path/file.json", { message: ''+message+'' });
-	} catch (error) {
-	  console.log(error)
-	}
-}
-
-async function mySkyExample() {
-	try {
-	  // Initialize MySky.
-	  mySky = await client.loadMySky(hostApp);
-	} catch (error) {
-	  console.log(error)
-	}
-  }
 
 
-
-(async () => {
-	$("#save_note").click(function() {
-		
-		$(".no-send").hide()
-		$(".sending").show()
-		console.log("ez meg lett nyomva")
-	  let message=$("#note").val()
-	    setJSONExample(message).then(function(){
+$("#save_note").click(function() {
+	$(".no-send").hide()
+	$(".sending").show()
+	let message = $("#note").val()
+	setJSON(message).then(function(data){
+		if (data) {
 			$(".no-send").show()
 			$(".sending").hide()
-		})
-	});
-	mySky = await client.loadMySky(hostApp, {
-		debug: true,
-		dev: true,
-	})
-	let proba = await mySkyExample()
-	let proba1 = await loadDacsExample().then(function(result){
-		//document.getElementsByClassName("hide-if-initialized").style.display ="visible"
-		$(".hide-if-initialized").hide()
-		$(".show-if-initialized").show()
-		
-		})
-
-	await requestLoginAccessExample()
-	isLoggedIn = await mySky.checkLogin()
-	console.log(isLoggedIn)
-
-	if (isLoggedIn) {
-		userId = await mySky.userID()
-		await loadDacsExample()
-		//await setJSONExample()
-		let Message=await getJSONExample()
-		if(Message!=null){
-			$("#note").val(Message.message);
+		} else {
+			alert('save failed')
 		}
-		$(".hide-if-logged-in").hide()
-		$(".show-if-logged-in").show()
-		
+	})
+});
+
+$("#logout").click(function() {
+	logout()
+});
+
+$("#login-button").click(function() {
+	login()
+});
+
+
+// define async setup function
+async function initMySky() {
+	try {
+		// load invisible iframe and define app's data domain
+		// needed for permissions write
+		mySky = await client.loadMySky(hostApp, {
+			debug: false,
+			dev: true,
+		});
+  
+		// load necessary DACs and permissions
+		// await mySky.loadDacs(contentRecord);
+		const dac = new ContentRecordDAC()
+		let dacData = await mySky.loadDacs(dac)
+		console.log('dacData', dacData)
+	
+		// check if user is already logged in with permissions
+	} catch (e) {
+		console.error(e);
 	}
+}
+  
+
+  
+
+(async () => {
+
+	// call async setup function
+	await initMySky()
+	$(".hide-if-initialized").hide()
+	$(".show-if-initialized").show()
+	await checkLogin()
+
 })();
 
